ca943d67b98af2dc007f8412116f91fb57b3d96d
dima <hrapovd@gmail.com>
2019-06-06 17:46:03 +0300

    Исправил ошибку в скрипте

--- master/k8s/k8s-install.sh
+++ master/k8s/k8s-install.sh
@@ -4,7 +4,7 @@ if [ $EUID -eq 0 ];then
     curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
     cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
     deb https://apt.kubernetes.io/ kubernetes-xenial main
-    EOF
+EOF
     apt-get update
     apt-get install -y kubelet kubeadm kubectl
     apt-mark hold kubelet kubeadm kubectl
