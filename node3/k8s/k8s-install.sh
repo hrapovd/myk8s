e7fa981525e9dc7439325b26532d89440137221d
dima <hrapovd@gmail.com>
2019-06-06 17:42:15 +0300

    Добавил скрипт установки kubeadm kubectl kubelet

---
+++ node3/k8s/k8s-install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+if [ $EUID -eq 0 ];then
+    apt-get update && apt-get install -y apt-transport-https curl
+    curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+    cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+    deb https://apt.kubernetes.io/ kubernetes-xenial main
+    EOF
+    apt-get update
+    apt-get install -y kubelet kubeadm kubectl
+    apt-mark hold kubelet kubeadm kubectl
+else
+    echo "You need run it as root."
+fi
